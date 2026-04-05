49a802f6fa7e8c7b9e6a0fa13b5c2d30accb3326
juliangranda <juliangranda@hotmail.com>
2026-04-05 18:06:31 -0500

    sesion state para tablas

--- pages/tablas.py
+++ pages/tablas.py
@@ -6,40 +6,42 @@ st.title(":material/table_chart: database", text_alignment= "center")
 
 df = datos.dataset()
 
-# --- SECCIÓN 1: ESTRÉS VS SUEÑO (Comparativa) ---
-col1, col2 = st.columns(2)
+if "df_filtrado" in st.session_state:
+    df = st.session_state["df_filtrado"]
+    # --- SECCIÓN 1: ESTRÉS VS SUEÑO (Comparativa) ---
+    col1, col2 = st.columns(2)
 
-with col1:
-    st.subheader("🔥 Nivel de Estrés", text_alignment="center")
-    # Simplificamos el cálculo
-    df_estres = df.groupby('occupation')['stress level'].mean().sort_values(ascending=False).reset_index()
-    # Usamos st.dataframe con un ajuste de ancho automático
-    st.dataframe(df_estres.rename(columns={'stress level': 'Estrés Promedio'}), hide_index=True, width = "stretch")
+    with col1:
+        st.subheader("🔥 Nivel de Estrés", text_alignment="center")
+        # Simplificamos el cálculo
+        df_estres = df.groupby('occupation')['stress level'].mean().sort_values(ascending=False).reset_index()
+        # Usamos st.dataframe con un ajuste de ancho automático
+        st.dataframe(df_estres.rename(columns={'stress level': 'Estrés Promedio'}), hide_index=True, width = "stretch")
 
-with col2:
-    st.subheader("😴 Duración de Sueño", text_alignment= 'center')
-    df_sueno = df.groupby('occupation')['sleep duration'].mean().sort_values(ascending=False).reset_index()
-    st.dataframe(df_sueno.rename(columns={'sleep duration': 'Horas Promedio'}), hide_index=True, width = "stretch")
+    with col2:
+        st.subheader("😴 Duración de Sueño", text_alignment= 'center')
+        df_sueno = df.groupby('occupation')['sleep duration'].mean().sort_values(ascending=False).reset_index()
+        st.dataframe(df_sueno.rename(columns={'sleep duration': 'Horas Promedio'}), hide_index=True, width = "stretch")
 
-st.divider()
+    st.divider()
 
-# --- SECCIÓN 2: ACTIVIDAD FÍSICA DETALLADA ---
-st.subheader("🏃‍♂️ Actividad Física "  , text_alignment="center")
+    # --- SECCIÓN 2: ACTIVIDAD FÍSICA DETALLADA ---
+    st.subheader("🏃‍♂️ Actividad Física "  , text_alignment="center")
 
-df_exercise = df.groupby('occupation').agg({
-    'physical activity level': ['min', 'max', 'mean'],
-    'stress level': 'mean'
-}).round(2).reset_index()
+    df_exercise = df.groupby('occupation').agg({
+        'physical activity level': ['min', 'max', 'mean'],
+        'stress level': 'mean'
+    }).round(2).reset_index()
 
-# Ordenamos por estrés para ver quiénes están "peor"
-df_exercise.columns = [
-    'Ocupación', 
-    'Act.Mín', 'Act.Máx', 'Act.Prom',
-    'Estres.Prom'
-]
-df_exercise = df_exercise.sort_values('Estres.Prom', ascending=False)
-st.dataframe(df_exercise, hide_index=True, width = "stretch")
+    # Ordenamos por estrés para ver quiénes están "peor"
+    df_exercise.columns = [
+        'Ocupación', 
+        'Act.Mín', 'Act.Máx', 'Act.Prom',
+        'Estres.Prom'
+    ]
+    df_exercise = df_exercise.sort_values('Estres.Prom', ascending=False)
+    st.dataframe(df_exercise, hide_index=True, width = "stretch")
 
-# Tip Junior: Un pequeño mensaje que conecte los datos
-st.info("**Nota:** Compara cómo las profesiones con más estrés (arriba a la izquierda) suelen coincidir con menos horas de actividad física.")
+    # Tip Junior: Un pequeño mensaje que conecte los datos
+    st.info("**Nota:** Compara cómo las profesiones con más estrés (arriba a la izquierda) suelen coincidir con menos horas de actividad física.")
 
